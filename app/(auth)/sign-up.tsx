2a56624188642d87ebf48f490a73edda93c55468
Lipkowsky <piotr.lipkowsky@gmail.com>
2024-09-17 12:31:11 +0200

    STARTER OF REACT NATIVE APP

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,36 @@
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { icons, images } from "@/constans";
+import InputField from "@/components/InputField";
+import { useState } from "react";
+
+const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create you Account
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default SignUp;
